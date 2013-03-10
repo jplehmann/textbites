5a3f70f398aa4507bef0e53310f66235f810e4df
John Lehmann <john.lehmann@gmail.com>
2013-03-10 16:16:15 -0500

    reorganized tests into interface and implementation tests

--- README.md
+++ README.md
@@ -3,7 +3,14 @@ pyBooks
 =======
 
 
-TODO
+Questions
+---------
+- how to use same set of tests, with a different implementation without rewriting the tests
+  - answer: use a getter in each test to obtain the implementatation
+  - I'm thinking you'd want a parent testsets which calls getImplUnderTest() which is provided
+    by either subclass. Then run the subclass tests
+
+Todo
 ----
 - Tests
   - assert parent types in test
@@ -11,10 +18,15 @@ TODO
 
 
 - What if we used the non-view approach?
-- Consider whether Line and Lines are confusing, or inconsistent?
 - Could possibly simplify by not using array in the resource, to help with off by one
   - and/or we could use arrays with a dummy element
 - Should I move this into pybible or a common project?
+
+- Consider whether Line and Lines are confusing, or inconsistent?
 - is it bad that chapter refs returns an array which is 0 indexed?
   - possibly should return Chapters() reference
+  1) I think the problem is that Chapter.children() does not return an array 
+    but everything else does.
+    Book -> Chapter -> Lines -> Line
+  2) returning arrays may also be bad
 
--- test/test_books.py
+++ test/test_books.py
@@ -9,92 +9,59 @@ import os.path
 import json
 
 from books.api import Resource, Reference
-from books.books import BookResource, Book, Chapter, Lines, Line
+from books.books import BookResource, Line
 from books import library as Library
 
 
 DATA_FILE = os.path.join(os.path.dirname(__file__), "../data/pp-sample.json")
 
 
-class TestBooks(unittest.TestCase):
+class TestInterface():
+  """ Tests the contract for the interfaces Resource and Reference.
+  Subclass this and define self.res as the resource.
+  """
 
   @classmethod
-  def setUpClass(cls): 
-    TestBooks.data = json.load(open(DATA_FILE))
-    print TestBooks.data
+  def setUpClass(cls):
+    TestInterface.data = json.load(open(DATA_FILE))
+    print TestInterface.data
 
-  def setUp(self):
-    self.bres = BookResource.from_json(TestBooks.data)
-
-  def test_library(self):
-    r = Resource()
-    self.assertEqual(len(Library.list()), 0)
-    Library.add("r1", r)
-    self.assertEqual(len(Library.list()), 1)
-    r1 = Library.get("r1")
-    self.assertEqual(r1, r)
-
+  # API: Reference tests ...
+  #{{{
   def test_parse_chapter_reference(self):
-    ref = self.bres.reference("Chapter 2")
+    ref = self.res.reference("Chapter 2")
     self.assertIsInstance(ref, Reference)
     self.assertEquals(ref.pretty(), "Chapter 2")
 
   def test_parse_chapter_reference_lower(self):
-    ref = self.bres.reference("chapter 2")
+    ref = self.res.reference("chapter 2")
     self.assertEquals(ref.pretty(), "Chapter 2")
 
   def test_parse_chapter_reference_without_chapter(self):
-    ref = self.bres.reference("2")
+    ref = self.res.reference("2")
     self.assertEquals(ref.pretty(), "Chapter 2")
 
   def test_parse_chapter_reference_with_one_line(self):
-    ref = self.bres.reference("2:1")
+    ref = self.res.reference("2:1")
     self.assertEquals(ref.pretty(), "Chapter 2:1")
 
   def test_parse_chapter_reference_with_start_and_end_lines(self):
-    ref = self.bres.reference("2:1-3")
+    ref = self.res.reference("2:1-3")
     self.assertIsInstance(ref, Reference)
     self.assertEquals(ref.pretty(), "Chapter 2:1-3")
+  #}}}
 
-  def test_chapter_refs(self):
-    chaps = self.bres.chapter_refs()
-    self.assertEquals(len(chaps), 3)
+  # API: Resource. top_reference()
+  def test_top_reference(self):
+    ref = self.res.top_reference()
+    self.assertIsInstance(ref, Reference)
 
-  def test_chapter_length(self):
-    chaps = self.bres.chapter_refs()
-    c3 = chaps[2]
-    self.assertEquals(self.bres.chapter_length(c3.num()), 7)
-
-  def test_chapter_text_for_whole_chapter_default(self):
-    chaps = self.bres.chapter_refs()
-    c3 = chaps[2]
-    text = self.bres.chapter_text(c3.num())
-    self.assertEquals(len(text), 817)
-
-  def test_chapter_text_for_whole_chapter_from_one(self):
-    chaps = self.bres.chapter_refs()
-    c3 = chaps[2]
-    text = self.bres.chapter_text(c3.num(), first_line=1)
-    self.assertEquals(len(text), 817)
-
-  def test_chapter_text_for_whole_chapter_to_last(self):
-    chaps = self.bres.chapter_refs()
-    c3 = chaps[2]
-    last = self.bres.chapter_length(c3.num())
-    text = self.bres.chapter_text(c3.num(), last_line=last)
-    self.assertEquals(len(text), 817)
-
-  def test_chapter_text_for_whole_chapter_from_first_to_last(self):
-    chaps = self.bres.chapter_refs()
-    c3 = chaps[2]
-    last = self.bres.chapter_length(c3.num())
-    text = self.bres.chapter_text(c3.num(), first_line=1, last_line=last)
-    self.assertEquals(len(text), 817)
-
+  # API: Reference. children(), pretty(), and text()
+#{{{
   def test_book_refs(self):
-    book = self.bres.top_reference()
-    self.assertIsInstance(book, Book)
+    book = self.res.top_reference()
     self.assertIsInstance(book, Reference)
+    #self.assertIsInstance(book, Book)
     self.assertEquals(book.pretty(), "PRIDE AND PREJUDICE")
     # cannot get text on a book
     self.assertRaises(NotImplementedError, book.text)
@@ -103,13 +70,13 @@ class TestBooks(unittest.TestCase):
     self.assertEquals(len(chaps), 3)
     c3 = chaps[2]
     self.assertIsInstance(c3, Reference)
-    self.assertIsInstance(c3, Chapter)
+    #self.assertIsInstance(c3, Chapter)
     self.assertEquals(c3.pretty(), "Chapter 3")
     self.assertEquals(len(c3.text()), 817)
 
     lines = c3.children()
-    self.assertIsInstance(lines, Lines)
     self.assertIsInstance(lines, Reference)
+    #self.assertIsInstance(lines, Lines)
     self.assertEquals(lines.pretty(), "Chapter 3:1-7")
     self.assertEquals(len(lines.text()), 817)
 
@@ -117,12 +84,15 @@ class TestBooks(unittest.TestCase):
     self.assertEquals(len(line_array), 7)
     one_line = line_array[-1]
     self.assertIsInstance(one_line, Reference)
-    self.assertIsInstance(one_line, Line)
+    #self.assertIsInstance(one_line, Line)
     self.assertEquals(one_line.pretty(), "Chapter 3:7")
     self.assertEquals(len(one_line.text()), 128)
+#}}}
 
+  # API: Reference. search()
+#{{{
   def test_book_search(self):
-    book = self.bres.top_reference()
+    book = self.res.top_reference()
     hits = book.search("daughter")
     self.assertEquals(len(hits), 3)
     self.assertIsInstance(hits[0], Line)
@@ -134,7 +104,7 @@ class TestBooks(unittest.TestCase):
     self.assertTrue(hits[2].text().endswith("of Mr. Bingley."))
 
   def test_book_search_one_chapter(self):
-    book = self.bres.top_reference()
+    book = self.res.top_reference()
     chaps = book.children()
     c3 = chaps[2]
     hits = c3.search("daughter")
@@ -142,40 +112,97 @@ class TestBooks(unittest.TestCase):
     self.assertEquals(hits[0].pretty(), "Chapter 3:1")
 
   def test_book_search_from_chapter_to_end(self):
-    hits = self.bres.search("daughter", first_chapter=2)
+    hits = self.res.search("daughter", first_chapter=2)
     self.assertEquals(len(hits), 2)
     self.assertEquals(hits[0].pretty(), "Chapter 2:4")
 
   def test_book_search_to_chapter_to_end(self):
-    hits = self.bres.search("daughter", last_chapter=2)
+    hits = self.res.search("daughter", last_chapter=2)
     self.assertEquals(len(hits), 2)
     self.assertEquals(hits[0].pretty(), "Chapter 1:2")
 
   def test_book_search_line_range(self):
-    hits = self.bres.search("Mr\.", first_chapter=2, last_chapter=2, first_line=1, last_line=6)
+    hits = self.res.search("Mr\.", first_chapter=2, last_chapter=2, first_line=1, last_line=6)
     self.assertEquals(len(hits), 3)
     self.assertEquals(hits[0].pretty(), "Chapter 2:1")
     self.assertEquals(hits[1].pretty(), "Chapter 2:5")
     self.assertEquals(hits[2].pretty(), "Chapter 2:6")
 
   def test_book_search_from_line(self):
-    hits = self.bres.search("Mr\.", first_chapter=2, last_chapter=2, first_line=2, last_line=6)
+    hits = self.res.search("Mr\.", first_chapter=2, last_chapter=2, first_line=2, last_line=6)
     self.assertEquals(len(hits), 2)
     self.assertEquals(hits[0].pretty(), "Chapter 2:5")
 
   def test_book_search_to_line(self):
-    hits = self.bres.search("Mr\.", first_chapter=2, last_chapter=2, first_line=1, last_line=5)
+    hits = self.res.search("Mr\.", first_chapter=2, last_chapter=2, first_line=1, last_line=5)
     self.assertEquals(len(hits), 2)
     self.assertEquals(hits[1].pretty(), "Chapter 2:5")
 
   def test_book_search_of_lines(self):
-    lines = self.bres.reference("2:1-5")
+    lines = self.res.reference("2:1-5")
     hits = lines.search("Mr\.")
     self.assertEquals(len(hits), 2)
     self.assertEquals(hits[1].pretty(), "Chapter 2:5")
-
+#}}}
   
-if __name__ == "__main__":
-  unittest.main()
+
+class TestLibrary(unittest.TestCase):
+
+  def test_library(self):
+    r = Resource()
+    self.assertEqual(len(Library.list()), 0)
+    Library.add("r1", r)
+    self.assertEqual(len(Library.list()), 1)
+    r1 = Library.get("r1")
+    self.assertEqual(r1, r)
+
+
+class TestBooksImpl(TestInterface, unittest.TestCase):
+  """ Create the implementation-specific system under test which 
+  is the Resource.
+  """
+
+  def setUp(self):
+    self.res = BookResource.from_json(TestInterface.data)
+
+  # IMPL: chapter-specific tests
+#{{{
+  def test_chapter_refs(self):
+    chaps = self.res.chapter_refs()
+    self.assertEquals(len(chaps), 3)
+
+  def test_chapter_length(self):
+    chaps = self.res.chapter_refs()
+    c3 = chaps[2]
+    self.assertEquals(self.res.chapter_length(c3.num()), 7)
+
+  def test_chapter_text_for_whole_chapter_default(self):
+    chaps = self.res.chapter_refs()
+    c3 = chaps[2]
+    text = self.res.chapter_text(c3.num())
+    self.assertEquals(len(text), 817)
+
+  def test_chapter_text_for_whole_chapter_from_one(self):
+    chaps = self.res.chapter_refs()
+    c3 = chaps[2]
+    text = self.res.chapter_text(c3.num(), first_line=1)
+    self.assertEquals(len(text), 817)
+
+  def test_chapter_text_for_whole_chapter_to_last(self):
+    chaps = self.res.chapter_refs()
+    c3 = chaps[2]
+    last = self.res.chapter_length(c3.num())
+    text = self.res.chapter_text(c3.num(), last_line=last)
+    self.assertEquals(len(text), 817)
+
+  def test_chapter_text_for_whole_chapter_from_first_to_last(self):
+    chaps = self.res.chapter_refs()
+    c3 = chaps[2]
+    last = self.res.chapter_length(c3.num())
+    text = self.res.chapter_text(c3.num(), first_line=1, last_line=last)
+    self.assertEquals(len(text), 817)
+#}}}
+
+
 
 
