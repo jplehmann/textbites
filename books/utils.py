a13673ea3dd4c89f811a58d4705b9f10d319e172
John Lehmann <john.lehmann@gmail.com>
2013-03-23 22:57:28 -0500

    factored out utilities

---
+++ books/utils.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def zero_indexed(val, none_val=None):
+  """ Convert this 1-based index to a 0-based one,
+      Also considering that None should not be modified.
+  """
+  return val-1 if val != None else none_val
+
+def increment(val, none_val=None):
+  return val+1 if val != None else none_val
+
+def val_or_default(val, none_val=0):
+  return val if val != None else none_val
+
