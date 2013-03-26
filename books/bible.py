715a9f27b83a845cb888db53da82198ba44839c8
John Lehmann <john.lehmann@gmail.com>
2013-03-26 05:38:00 -0500

    Added Bible book, not done.

---
+++ books/bible.py
@@ -0,0 +1,182 @@
+#! /usr/bin/env python
+"""
+Adapt Bible API into that which pybooks expects.
+"""
+import re
+
+from pybooks.api import Reference, Resource, UnparsableReferenceError, InvalidReferenceError
+from .utils import *
+import bibref
+import sys
+
+bibref.init()
+
+class BibleResource(Resource):
+
+  @staticmethod
+  def from_txt(data):
+    # TODO
+    """ Create a resource from json data.
+        Assumes title, author, chapters/text
+    """
+    #chapters = []
+    #for cnum, chapter in enumerate(data.get("chapters"), 1):
+    #  lines = []
+    #  for lnum, line in enumerate(chapter.get("text").split('\n'), 1):
+    #    lines.append(Line(cnum, lnum, line))
+    #  chapters.append(Chapter(cnum, lines))
+    #title = data.get("title")
+    #author = data.get("author")
+    #return SimpleBookResource(Book(chapters, title, author))
+
+  def __init__(self, book):
+    """ Stores only the top reference.
+    """
+    self.books = books
+
+  def reference(self, str_ref):
+    """ Parse this string reference and return an object. 
+    """
+    m = re.match("(?:[Cc]hapter ?)?(\d+)(?:-(\d+))?(?::(\d+)(?:-(\d+))?)?", str_ref)
+    if m:
+      chap_start = m.group(1)
+      chap_end = m.group(2)
+      start = m.group(3)
+      fc = zero_indexed(int(chap_start))
+      if not start:
+        if not chap_end:
+          return self.book.children()[fc]
+        else:
+          if int(chap_end) > len(self.book.children()):
+            raise InvalidReferenceError()
+          return ChapterRange(
+              self.book.children()[fc:int(chap_end)])
+      end = m.group(4)
+      chapter = self.book.children()[fc]
+      if not end:
+        # leverage LineRange to extract a line
+        return LineRange(chapter, int(start), int(start)).children()[0]
+      return LineRange(chapter, int(start), int(end))
+    raise UnparsableReferenceError()
+
+  def top_reference(self):
+    return self.book
+
+
+class Book(Reference):
+  """ A single book.
+  """
+  def __init__(self, chapters, title=None, author=None):
+    self.chapters = chapters
+    self.author = author
+    self.title = title
+
+  def children(self):
+    return self.chapters
+
+  def pretty(self):
+    return self.title
+
+  def text(self):
+    """ Too much text. """
+    raise NotImplementedError()
+
+  def search(self, pattern, first_chapter=None, last_chapter=None, 
+                            first_line=None, last_line=None):
+    hits = []
+    fc = zero_indexed(first_chapter)
+    for chap in self.chapters[fc:last_chapter]:
+      hits.extend(chap.search(pattern, first_line, last_line))
+    return hits
+
+
+class ChapterRange(Reference):
+  """ A range of chapters.
+  """
+  def __init__(self, chapters):
+    self.chapters = chapters
+
+  def children(self):
+    return self.chapters
+
+  def pretty(self):
+    return "Chapter %d-%d" % (self.chapters[0].num, self.chapters[-1].num)
+
+  def text(self):
+    raise NotImplementedError()
+
+  def search(self, pattern, first_line=None, last_line=None):
+    hits = []
+    for chap in self.chapters:
+      hits.extend(chap.search(pattern))
+    return hits
+
+
+class Chapter(Reference):
+  """ A single chapter.
+  """
+  def __init__(self, num, lines):
+    self.num = num
+    self.lines = lines 
+
+  def children(self):
+    return self.lines
+
+  def pretty(self):
+    return "Chapter %d" % self.num
+
+  def text(self):
+    return '\n'.join([l.text() for l in self.lines]).strip()
+
+  def search(self, pattern, first_line=None, last_line=None):
+    fl = zero_indexed(first_line)
+    return [l for l in self.lines[fl:last_line] if l.search(pattern)]
+
+
+class LineRange(Reference):
+  """ A range of lines.
+  """
+  def __init__(self, chapter, start, end):
+    if end > len(chapter.children()):
+      raise InvalidReferenceError()
+    self.chapter = chapter
+    self.start = start
+    self.end = end
+
+  def children(self):
+    return self.chapter.children()[zero_indexed(self.start):self.end]
+
+  def pretty(self):
+    s = self.chapter.pretty() + ":%d" % self.start
+    return s if self.start == self.end else s+"-%d" % self.end
+
+  def text(self):
+    raise NotImplementedError()
+
+  def search(self, pattern):
+    return self.chapter.search(pattern, self.start, self.end)
+
+
+class Line(Reference):
+  """ A single line.
+  """
+  def __init__(self, cnum, lnum, line):
+    self.line = line
+    self.cnum = cnum
+    self.lnum = lnum
+
+  def children(self):
+    raise NotImplementedError()
+
+  def pretty(self):
+    return "Chapter %d:%d" % (self.cnum, self.lnum)
+
+  def text(self):
+    return self.line
+
+  def search(self, pattern):
+    if re.search(pattern, self.line):
+      return self
+    return None
+
+
