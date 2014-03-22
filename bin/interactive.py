84daddd816344da740b32e8ca3d83b6ed187905b
John Lehmann <john.lehmann@gmail.com>
2014-03-22 11:02:45 -0500

    Format block vs line mode.

--- bin/interactive.py
+++ bin/interactive.py
@@ -57,6 +57,8 @@ def main(args):
   print "Setting resource to:", cur_resname()
   # for searching
   context = [cur_resource.top_reference()]
+  # prefer single lines to block
+  one_per_line = False
 
   def context_str():
     return [str(c.pretty()) for c in context]
@@ -82,35 +84,36 @@ def main(args):
       print
     print "Displayed", len(results), "results."
 
+  def format_lines(ref):
+    def ref_str(v):
+      return "{}:{}".format(v.parent().short(), v.short())
+    v_format = "{ref} {text}"
+    lines = ref.children() or [ref]
+    text="\n".join([v_format.format(ref=ref_str(v), text=v.text())
+      for v in lines])
+    return text
+
+  def format_block(ref):
+    # show reference per line if there are > 10
+    v_format = ("{text}" if not ref.children() or len(ref.children()) < 10 
+                           else "{ref} {text}")
+    lines = ref.children() or [ref]
+    text = " ".join([v_format.format(ref=v.short(), text=v.text())
+        for v in lines])
+    ref_str = " ({ref} {res})".format(ref=ref.pretty(), res=cur_resname())
+    return text + ref_str
+
   def display_one_ref(ref):
     try:
-      text = ref.text()
-    except:
-      text = None
-    # verse(s)/line(s)
-    if text and not ref.children():
-      print "{text} ({ref} {res})".format(
-        text=ref.text(), 
-        ref=ref.pretty(),
-        res=cur_resname()
-      )
-    # multiple verses or one chapter
-    elif text and ref.children():
-      v_format = "{text}" if len(ref.children()) < 10 else "{num} {text}"
-      print "{text} ({ref} {res})".format(
-        text=" ".join([v_format.format(num=v.short(), text=v.text()) 
-                                           for v in ref.children()]),
-        ref=ref.pretty(),
-        res=cur_resname()
-      )
-    # book or chapters
-    elif not text:
+      ref.text()
+      # verse, multiple verses or one chapter
+      print format_lines(ref) if one_per_line else format_block(ref)
+    except Exception as e:
+      print e
+      # book or chapters
       print "Setting context to:", ref.pretty()
       del context[1:]
       context[0] = ref
-    else:
-      # search terms
-      print "Not sure what this is", ref
 
   # REPL Loop.
   while True:
@@ -123,11 +126,19 @@ def main(args):
     elif query in BOOK_GROUPS:
       context = [cur_resource.reference(b) for b in BOOK_GROUPS.get(query)]
       print "Setting resource to:", context_str()
+    elif query.startswith("format"):
+      if query == "format lines":
+        one_per_line = True
+      elif query == "format block":
+        one_per_line = False
+      else:
+        print "Say 'format lines' or 'format block'"
     elif len(query.strip()):
       try:
         ref = cur_resource.reference(query)
         display_one_ref(ref)
-      except:
+      except Exception as e:
+        print e
         search(query)
 
 if __name__ == "__main__":
